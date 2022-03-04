4659e7891092e86df6e5ff8f7b18bd428de5db24
Ittigorn Tradussadee <ittigorn.tra@gmail.com>
2022-03-04 23:27:50 +0700

    Edited roman digit conversion script

--- ex03_algorithmic_test/main.py
+++ ex03_algorithmic_test/main.py
@@ -1,31 +1,4 @@
-ARABIC_ROMAN_DIGIT_MAP = [
-    {"arabic": 1000000, "roman": "_M"},
-    {"arabic": 900000, "roman": "_C_M"},
-    {"arabic": 500000, "roman": "_D"},
-    {"arabic": 400000, "roman": "_C_D"},
-    {"arabic": 100000, "roman": "_C"},
-    {"arabic": 90000, "roman": "_X_C"},
-    {"arabic": 50000, "roman": "_L"},
-    {"arabic": 40000, "roman": "_X_L"},
-    {"arabic": 10000, "roman": "_X"},
-    {"arabic": 9000, "roman": "_I_X"},
-    {"arabic": 5000, "roman": "_V"},
-    {"arabic": 5000, "roman": "_V"},
-    {"arabic": 4000, "roman": "_I_V"},
-    {"arabic": 1000, "roman": "M"},
-    {"arabic": 900, "roman": "CM"},
-    {"arabic": 500, "roman": "D"},
-    {"arabic": 400, "roman": "CD"},
-    {"arabic": 100, "roman": "C"},
-    {"arabic": 90, "roman": "XC"},
-    {"arabic": 50, "roman": "L"},
-    {"arabic": 40, "roman": "XL"},
-    {"arabic": 10, "roman": "X"},
-    {"arabic": 9, "roman": "IX"},
-    {"arabic": 5, "roman": "V"},
-    {"arabic": 4, "roman": "IV"},
-    {"arabic": 1, "roman": "I"},
-]
+from config import ARABIC_ROMAN_DIGIT_MAP, NULL_REPRESENTATION
 
 
 def to_roman_digits(value: int) -> str:
@@ -34,24 +7,27 @@ def to_roman_digits(value: int) -> str:
     if value < 0:
         raise ValueError('Roman Numerals does not include negative numbers')
     if value == 0:
-        return 'nulla'
+        return NULL_REPRESENTATION
 
+    start_at = 0
     result = []
 
     # loop while not depleted
     while value > 0:
-        for digit_pair in ARABIC_ROMAN_DIGIT_MAP:
+        for digit_pair in ARABIC_ROMAN_DIGIT_MAP[start_at:]:
             # find value that subtract to zero, or more than 0, descending
             if (value - digit_pair['arabic']) >= 0:
                 result.append(digit_pair['roman'])
                 value = value - digit_pair['arabic']
                 break
+            else:
+                start_at += 1
 
     return ''.join(result)
 
 
 if __name__ == '__main__':
-    value = 11000
+    value = 2253
 
     result = to_roman_digits(value=value)
     print(result)
---
+++ ex03_algorithmic_test/tests/test_main.py
@@ -0,0 +1,41 @@
+from logging import getLogger
+
+import pytest
+from main import to_roman_digits
+
+logger = getLogger()
+
+
+@pytest.fixture
+def good_values():
+    return [
+        [1, 'I'],
+        [4, 'IV'],
+        [9, 'IX'],
+        [10, 'X'],
+        [11, 'XI'],
+        [21, 'XXI'],
+        [40, 'XL'],
+        [2253, 'MMCCLIII'],
+        [361054, '_C_C_C_L_XMLIV'],
+        [667189, '_D_C_L_X_VMMCLXXXIX'],
+        [285667, '_C_C_L_X_X_X_VDCLXVII'],
+        [126266, '_C_X_X_VMCCLXVI'],
+        [188051, '_C_L_X_X_X_VMMMLI'],
+        [445782, '_C_D_X_L_VDCCLXXXII'],
+        [162972, '_C_L_XMMCMLXXII'],
+        [660787, '_D_C_L_XDCCLXXXVII'],
+        [64455, '_L_X_I_VCDLV'],
+        [91430, '_X_CMCDXXX'],
+    ]
+
+
+def test_good_values(good_values):
+    for test_value, expected_result in good_values:
+        roman_digits = to_roman_digits(test_value)
+        logger.info(f'Test Value : {str(test_value).rjust(8)} Roman Digits : {roman_digits}')
+
+        # check if return type is as in the requirement
+        assert isinstance(roman_digits, str)
+        assert len(roman_digits) > 0
+        assert roman_digits == expected_result
